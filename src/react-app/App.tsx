6ef6a852958b3cb1c75d794ed207f975f64ed0bc
Actualización Docente <coord_actualizaciondocente@itdurango.edu.mx>
2025-09-28 20:18:42 -0600

    Add files via upload

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,10 +1,10 @@
-import { HashRouter as Router, Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "@/react-app/pages/Home";
 import AdminPage from "@/react-app/pages/Admin";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/DA-itd-web/">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/admin" element={<AdminPage />} />
