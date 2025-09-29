132f7541317b9340b0f7151a883d3048a8b5aad2
Actualización Docente <coord_actualizaciondocente@itdurango.edu.mx>
2025-09-28 20:54:02 -0600

    Add files via upload

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,10 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { useMemo } from "react";
 import HomePage from "@/react-app/pages/Home";
 import AdminPage from "@/react-app/pages/Admin";
 
 export default function App() {
+  const basename = useMemo(() => "/web/", []); // <-- ¡Agrega esta línea!
+
   return (
-    <Router basename="/DA-itd-web/">
+    <Router basename={basename}> // <-- ¡Modifica esta línea!
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/admin" element={<AdminPage />} />
