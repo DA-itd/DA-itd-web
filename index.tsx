f26b826d77780b5c15fb27a7cd8fde94934b5b39
Actualización Docente <coord_actualizaciondocente@itdurango.edu.mx>
2025-10-05 20:42:33 -0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,10 +1,9 @@
+import "./index.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement!);
-
+const root = createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <App />
